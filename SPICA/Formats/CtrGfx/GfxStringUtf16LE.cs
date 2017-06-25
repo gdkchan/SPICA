0042770c57506dd368aa7c0bf1a6cf3dd9c9e9cd
gdkchan <gab.dark.100@gmail.com>
2017-06-25 15:07:24 -0300

    Aaaand I forgot to save this...

--- SPICA/Formats/CtrGfx/GfxStringUtf16LE.cs
+++ SPICA/Formats/CtrGfx/GfxStringUtf16LE.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SPICA.Formats.Common;
+using SPICA.Serialization;
+using SPICA.Serialization.Attributes;
 
 namespace SPICA.Formats.CtrGfx
 {
-    class GfxStringUtf16LE
+    public class GfxStringUtf16LE : ICustomSerialization
     {
+        [Ignore] private string Str;
+
+        public GfxStringUtf16LE() { }
+
+        public GfxStringUtf16LE(string Str)
+        {
+            this.Str = Str;
+        }
+
+        public override string ToString()
+        {
+            return Str ?? string.Empty;
+        }
+
+        void ICustomSerialization.Deserialize(BinaryDeserializer Deserializer)
+        {
+            Str = Deserializer.Reader.ReadNullTerminatedStringUtf16LE();
+        }
+
+        bool ICustomSerialization.Serialize(BinarySerializer Serializer)
+        {
+            Serializer.Writer.WriteNullTerminatedStringUtf16LE(Str);
+
+            return true;
+        }
     }
 }
--- SPICA/Formats/CtrGfx/GfxStringUtf8.cs
+++ SPICA/Formats/CtrGfx/GfxStringUtf8.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SPICA.Formats.Common;
+using SPICA.Serialization;
+using SPICA.Serialization.Attributes;
 
 namespace SPICA.Formats.CtrGfx
 {
-    class GfxStringUtf8
+    public class GfxStringUtf8 : ICustomSerialization
     {
+        [Ignore] private string Str;
+
+        public GfxStringUtf8() { }
+
+        public GfxStringUtf8(string Str)
+        {
+            this.Str = Str;
+        }
+
+        public override string ToString()
+        {
+            return Str ?? string.Empty;
+        }
+
+        void ICustomSerialization.Deserialize(BinaryDeserializer Deserializer)
+        {
+            Str = Deserializer.Reader.ReadNullTerminatedStringUtf8();
+        }
+
+        bool ICustomSerialization.Serialize(BinarySerializer Serializer)
+        {
+            Serializer.Writer.WriteNullTerminatedStringUtf8(Str);
+
+            return true;
+        }
     }
 }
